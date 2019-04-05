83f3505444ea7e069ca69afe860d430f442ad7dd
Karly Mareka <karly.mareka@gmail.com>
2019-04-05 01:54:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,7 @@ class Student
   def update
     sql = "UPDATE students SET name = ?, grade = ? 
           WHERE id = ?"
-    
+    DB[:conn].execute(sql, self.name, self.album, self.id)
   end 
   
   
